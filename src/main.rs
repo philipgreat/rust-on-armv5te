e2b724df0196ee3ce6b388384fde0b2de5a6780f
Philip Zhang <zhangxilai@doublechaintech.com>
2023-10-01 00:32:13 +0800

    🚀fine

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::{thread, time};
+use serialport::SerialPort;
 
 fn main() {
-    // Create a Duration of 20 seconds.
-    let twenty_seconds = time::Duration::from_secs(20);
-    
-    // Put the current thread to sleep for 20 seconds.
-    thread::sleep(twenty_seconds);//
+    // Open the serial port.
+    let mut port = SerialPort::open("/dev/usb0", 115200).unwrap();
 
-    // Print a message to the console.
-    println!("Slept for 20 seconds!");
+    // Create a buffer to store the incoming data.
+    let mut buffer = [0; 128];
+
+    // Read data from the serial port.
+    let bytes_read = port.read(&mut buffer).unwrap();
+
+    // Print the incoming data to the console.
+    println!("Received {} bytes: {}", bytes_read, String::from_utf8_lossy(&buffer[..bytes_read]));
 }
