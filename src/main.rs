969b35b85748f3fe500be63edc078992f8911672
Philip Zhang <zhangxilai@doublechaintech.com>
2023-10-01 00:18:18 +0800

    🚀fine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+
+
+use serialport::SerialPort;
+
+fn main() {
+    // Open the serial port.
+    let mut port = SerialPort::open("/dev/usb0", 115200).unwrap();
+
+    // Create a buffer to store the incoming data.
+    let mut buffer = [0; 128];
+
+    // Read data from the serial port.
+    let bytes_read = port.read(&mut buffer).unwrap();
+
+    // Print the incoming data to the console.
+    println!("Received {} bytes: {}", bytes_read, String::from_utf8_lossy(&buffer[..bytes_read]));
+}
+
+
+
+
+
+
+
+/*
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
@@ -11,3 +36,6 @@ fn main() {
     
     rocket::ignite().mount("/hello", routes![hello]).launch();
 }
+
+
+*/
