f8013e66e4da6a9adb1a545e79d60bd6f0ac4c23
Philip Zhang <zhangxilai@doublechaintech.com>
2023-10-01 00:28:08 +0800

    🚀fine

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,12 @@
-use serialport::prelude::*;
-use std::io::Read;
-use std::time::Duration;
+use std::{thread, time};
 
 fn main() {
-    let port_name = "/dev/usb0";
-    let s = SerialPortSettings {
-        baud_rate: 9600,
-        data_bits: DataBits::Eight,
-        flow_control: FlowControl::None,
-        parity: Parity::None,
-        stop_bits: StopBits::One,
-        timeout: Duration::from_millis(10),
-    };
+    // Create a Duration of 20 seconds.
+    let twenty_seconds = time::Duration::from_secs(20);
+    
+    // Put the current thread to sleep for 20 seconds.
+    thread::sleep(twenty_seconds);//
 
-    let mut port = serialport::open_with_settings(&port_name, &s).unwrap();
-
-    let mut serial_buf: Vec<u8> = vec![0; 1000];
-    println!("Receiving data...");
-
-    loop {
-        match port.read(serial_buf.as_mut_slice()) {
-            Ok(t) => {
-                for byte in serial_buf.iter().take(t) {
-                    print!("{}", *byte as char);
-                }
-            }
-            Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => (),
-            Err(e) => eprintln!("{:?}", e),
-        }
-    }
+    // Print a message to the console.
+    println!("Slept for 20 seconds!");
 }
