b7c012ec137cf8ab456c46f8a7da3ed259df2f18
Philip Zhang <zhangxilai@doublechaintech.com>
2023-10-01 00:44:29 +0800

    🚀fine

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,8 @@ use serialport::SerialPort;
 
 fn main() {
     // Open the serial port.
-    let mut port = <dyn SerialPort>::open("/dev/usb0", 115200).unwrap();
-
-    // Create a buffer to store the incoming data.
-    let mut buffer = [0; 128];
-
-    // Read data from the serial port.
-    let bytes_read = port.read(&mut buffer).unwrap();
-
-    // Print the incoming data to the console.
-    println!("Received {} bytes: {}", bytes_read, String::from_utf8_lossy(&buffer[..bytes_read]));
+    let ports = serialport::available_ports().expect("No ports found!");
+    for p in ports {
+        println!("{}", p.port_name);
+    }
 }
